3d855ffdaabadb80e46f36c508126a6791a634f7
Kerem Gocen (x-team) <ext.kgocen@riotgames.com>
2016-12-29 16:49:34 +0200

    async fetch
    - parametric config settings, db address, name etc. should be provided
    via a local config file
    - add async data fetching routine, the program will periodically fetch
    firebase data until its terminated. Fetching period is defined in
    config file.

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"strconv"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/keremgocen/mrmoody-metrics/storage"
@@ -19,79 +20,129 @@ func main() {
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
 
+	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("No configuration file loaded - using defaults")
+		log.Println("No configuration file loaded - using defaults")
+		// TODO defaults
 	}
 
 	// read config file
 	targetURL := viper.GetString("target_url")
 	apiKey := viper.GetString("api_key")
-	fmt.Printf("\n%s\n\n", viper.AllSettings())
+	period := viper.GetInt("update_period_minutes")
+	dbAddr := viper.GetString("db_address")
+	dbName := viper.GetString("db_name")
+	dbUser := viper.GetString("db_user")
+	dbPass := viper.GetString("db_pass")
+	log.Printf("\n%s\n\n", viper.AllSettings())
+
+	log.Println("Fetching period is set to", period, "seconds.")
 
 	// setup storage
-	c := storage.Setup()
+	c := storage.Setup(dbUser, dbPass, dbAddr)
 	defer c.Close()
-	storage.CreateDB()
+	err = storage.CreateDB(c, dbName)
+	if err != nil {
+		log.Println("failed to create database", err.Error())
+	}
+
+	// DEBUG TODO use seconds
+	dur := time.Minute * time.Duration(period)
+	ticker := time.NewTicker(dur)
+	done := make(chan bool, 1)
+	go func(done chan bool) {
+		for t := range ticker.C {
+			log.Println("tick", period, "at", t)
+
+			b, err := fetchFirebaseData(targetURL, apiKey)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+
+			res := storage.Votes{}
+			if err := json.Unmarshal([]byte(b), &res); err != nil {
+				log.Fatal(err)
+				break
+			}
+
+			log.Println("res:", res)
+
+			err2 := storage.WriteBatchPoints(c, res, dbName)
+			if err2 != nil {
+				log.Println("error writing points in db", err2.Error())
+			}
+			done <- true
+		}
+	}(done)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-stop)
+	ticker.Stop()
+	log.Println("Ticker stopped! Waiting for fetching routine..")
+
+	// Block until we receive a notification from the worker on the channel.
+	<-done
+
+	err = storage.Query(c, dbName)
+	if err != nil {
+		log.Println("error querying db", err.Error())
+	}
+
+}
+
+func fetchFirebaseData(URL, key string) ([]byte, error) {
 
 	// create request to parse Firebase data
-	req, err := http.NewRequest("GET", targetURL, nil)
+	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Add("Authentication", apiKey)
+	req.Header.Add("Authentication", key)
 
-	now := time.Now()
-	nowStr := strconv.FormatInt(unixMilli(now), 10)
-	fmt.Println("now:", now, " ts:", nowStr)
-	lastMonth := now.AddDate(0, -3, 0)
-	lastMonthStr := "1479495795942" //strconv.FormatInt(unixMilli(lastMonth), 10)
-	fmt.Println("last month:", lastMonth, " ts:", lastMonthStr)
+	// now := time.Now()
+	// nowStr := strconv.FormatInt(unixMilli(now), 10)
+	// log.Println("now:", now, " ts:", nowStr)
+	// lastMonth := now.AddDate(0, -3, 0)
+	// lastMonthStr := "1479495795942" //strconv.FormatInt(unixMilli(lastMonth), 10)
+	// log.Println("last month:", lastMonth, " ts:", lastMonthStr)
 
 	q := req.URL.Query()
 	// q.Add("orderBy", "\"date\"")
 	// q.Add("startAt", lastMonthStr)
 	// q.Add("endAt", nowStr)
-	q.Add("print", "pretty")
+	// q.Add("print", "pretty")
 	req.URL.RawQuery = q.Encode()
 
-	fmt.Println(req.URL.String())
+	log.Println(req.URL.String())
 
 	// log outgoing request
 	dump, err := httputil.DumpRequestOut(req, true)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Printf("%q\n", dump)
+	log.Printf("%q\n", dump)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println("Error reading results from ", targetURL)
-		fmt.Println("status ", resp.Status)
+		log.Println("error reading results from", URL)
+		log.Println("status", resp.Status)
 		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
 
-	// b = nil
-
-	// fmt.Printf("%s\n", b)
-
-	res := storage.Votes{}
-	if err := json.Unmarshal([]byte(b), &res); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("res:", res)
-
-	storage.WriteBatchPoints(c, res)
-	storage.Query(c)
-
+	return b, err
 }
 
 func unixMilli(t time.Time) int64 {
--- storage/vault101.go
+++ storage/vault101.go
@@ -1,17 +1,12 @@
 package storage
 
 import (
-	"fmt"
+	"log"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/influxdata/influxdb/client/v2"
-)
-
-const (
-	MyDB     = "test"
-	username = "kerem"
-	password = "test"
 )
 
 type User struct {
@@ -23,53 +18,49 @@ type Campaign map[string]User
 type Votes map[string]Campaign
 
 // Create a new client
-func Setup() client.Client {
+func Setup(user, pass, addr string) client.Client {
+
 	// NOTE: this assumes you've setup a user and have setup shell env variables,
 	// namely INFLUX_USER/INFLUX_PWD. If not just omit Username/Password below.
 	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: "http://localhost:8086",
+		Addr: addr,
 		// Username: os.Getenv("INFLUX_USER"),
 		// Password: os.Getenv("INFLUX_PWD"),
 	})
 	if err != nil {
-		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+		log.Println("error creating client", err.Error())
 	}
 
 	return c
 }
 
 // Create a Database with a query
-func CreateDB() {
-	// Make client
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: "http://localhost:8086",
-	})
-	if err != nil {
-		fmt.Println("Error creating InfluxDB Client: ", err.Error())
-	}
-	defer c.Close()
+func CreateDB(c client.Client, dbName string) error {
 
-	q := client.NewQuery("CREATE DATABASE test", "", "")
+	s := []string{"CREATE DATABASE "}
+	q := client.NewQuery(strings.Join(s, dbName), "", "")
 	if response, err := c.Query(q); err == nil && response.Error() == nil {
-		fmt.Println(response.Results)
+		log.Println(response.Results)
+	} else {
+		log.Println("error creating database", dbName)
+		return err
 	}
+	return nil
 }
 
 // Create a batch and add a point
-func WriteBatchPoints(c client.Client, p Votes) {
+func WriteBatchPoints(c client.Client, p Votes, dbName string) error {
 	// Create a new point batch
 	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "test",
+		Database:  dbName,
 		Precision: "s",
 	})
 
 	for k, value := range p {
-		fmt.Println("Campaign:", k, "details:", value)
+		log.Println("Campaign:", k, "details:", value)
 		tags := map[string]string{"campaign": k}
 
 		for key2, user := range value {
-			fmt.Println("User:", key2, "date:", user.Date, "mood:", user.Mood)
-
 			// Create a point and add to batch
 			fields := map[string]interface{}{
 				"user": key2,
@@ -78,32 +69,38 @@ func WriteBatchPoints(c client.Client, p Votes) {
 
 			pt, err := client.NewPoint("campaign", tags, fields, msToTime(strconv.FormatInt(user.Date, 10)))
 			if err != nil {
-				fmt.Println("Error: ", err.Error())
+				log.Println("error creating point", err.Error())
+				return err
 			}
 			bp.AddPoint(pt)
 		}
 	}
 
 	// Write the batch
-	c.Write(bp)
+	if err := c.Write(bp); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Make a Query
-func Query(c client.Client) {
-	q := client.NewQuery("SELECT * FROM campaign", "test", "s")
+func Query(c client.Client, dbName string) error {
+	q := client.NewQuery("SELECT * FROM campaign", dbName, "s")
 	if response, err := c.Query(q); err == nil && response.Error() == nil {
-		fmt.Println(response.Results)
+		log.Println("query results:", response.Results)
+		return nil
+	} else {
+		return err
 	}
 }
 
 func msToTime(ms string) time.Time {
 	msInt, err := strconv.ParseInt(ms, 10, 64)
 	if err != nil {
-		fmt.Println("Failed to parse", ms)
+		log.Println("error parsing duration", ms, err.Error())
 		return time.Time{}
 	}
-
-	// fmt.Println(time.Unix(0, msInt*int64(time.Millisecond)))
 
 	return time.Unix(0, msInt*int64(time.Millisecond))
 }
